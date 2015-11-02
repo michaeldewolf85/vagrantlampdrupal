72d61c3af53f93e2b1a3f1d7063582e4dcd2021c
Mike DeWolf <michaeldewolf85@gmail.com>
2015-11-02 14:04:14 -0500

    Instigate custom handling for languages based on path prefixes.

--- strap/php/setup.php
+++ strap/php/setup.php
@@ -13,3 +13,16 @@ $form_state['values']['language']['enabled']['locale-url'] = 1;
 $form_state['values']['language_content']['enabled']['locale-url'] = 1;
 $form_state['values']['op'] = 'Save settings';
 drupal_form_submit('locale_languages_configure_form', $form_state);
+
+// Configure Internationalization for Nodes.
+foreach (node_type_get_types() as $node_type) {
+  variable_set("language_content_type_{$node_type->type}", 4);
+}
+
+$lang_vars = array('language_negotiation_language', 'language_negotiation_language_content', 'language_negotiation_language_url');
+foreach ($lang_vars as $key) {
+  $var = variable_get($key);
+  $var['locale-url']['callbacks']['language'] = 'dakine_language_from_url';
+  $var['locale-url']['file'] = drupal_get_path('module', 'dakine_lang') . '/dakine_lang.module';
+  variable_set($key, $var);
+}
